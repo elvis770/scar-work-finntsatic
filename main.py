75b7eeef14e271422f4934bf2e182cbd8ff739d2
Elvis <105381818+elvis770@users.noreply.github.com>
2023-11-03 11:46:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+api_url = 'https://v1.nocodeapi.com/elvis/fit/QXjGNoJMMDbvvzgF/aggregatesDatasets?dataTypeName=calories_expended&timePeriod=7days'
+
+
+pyscript.write("next","yes here we go!!!")
